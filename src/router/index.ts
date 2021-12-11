074301db1fc13c647b57b49d4f355ebc0d67e3c3
naca-nyan <naca.nyan@gmail.com>
2021-12-11 21:48:41 +0900

    Get rid of About

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,15 +20,6 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('../views/BPM.vue'),
     meta: { title: 'BPM計るやつ'}
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-    meta: { title: 'About' }
-  }
 ]
 
 const router = createRouter({

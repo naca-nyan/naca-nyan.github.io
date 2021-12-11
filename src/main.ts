8a973b7c3ea0409d5763e29012fe7b78ac88e32f
naca-nyan <naca.nyan@gmail.com>
2021-12-11 17:09:24 +0900

    Add title

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const title_suffix = "naca-nyan.github.io"
+
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title + " - " + title_suffix
+    next()
+})
+
 createApp(App).use(router).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,8 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { title: 'Home' }
   },
   {
     path: '/about',
@@ -13,7 +14,8 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: { title: 'About' }
   }
 ]
 

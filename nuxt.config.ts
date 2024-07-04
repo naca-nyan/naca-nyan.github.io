81e8a36a95bcf04e73b2fe74ceb3ee65aa20dd0e
naca-nyan <naca.nyan@gmail.com>
2024-07-04 14:04:14 +0900

    :arrow_up: Upgrade nuxt to 3.12.3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
   app: {
     head: {
       title: "naca-nyan",
       titleTemplate: "%s - naca-nyan.github.io",
     },
   },
-  css: ["sakura.css/css/sakura-earthly.css", "@/css/style.css"],
+  css: ["sakura.css/css/sakura-earthly.css", "/css/style.css"],
 });

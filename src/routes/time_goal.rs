ca76c06f0110dc6aa7db6aaebc84a447c5cb00d4
naca-nyan <naca.nyan@gmail.com>
2026-04-08 22:58:02 +0900

    :sparkles: Consider break time

--- src/routes/time_goal.rs
+++ src/routes/time_goal.rs
@@ -3,14 +3,14 @@ use dioxus::prelude::*;
 
 use crate::TitleAndMeta;
 
-fn now() -> i32 {
+fn now_minutes() -> i32 {
     let now = chrono::Local::now();
     let min = now.hour() * 60 + now.minute();
     min as i32
 }
 
-fn try_from(value: &str) -> Result<i32, std::num::ParseIntError> {
-    let n: i32 = value.replace(':', "").parse()?;
+fn parse_as_minutes(s: &str) -> Result<i32, std::num::ParseIntError> {
+    let n: i32 = s.trim().replace(':', "").parse()?;
     let v = (n / 100) * 60 + (n % 100);
     Ok(v)
 }
@@ -21,19 +21,19 @@ fn fmt(v: i32) -> String {
 
 #[component]
 pub fn TimeGoal() -> Element {
-    let mut s = use_signal(String::new);
-    let mut goal_s = use_signal(|| String::from("8:00"));
-    let mut now_signal = use_signal::<Option<i32>>(|| None);
-    let mut times: Vec<i32> = s
+    let mut lines = use_signal(String::new);
+    let mut goal_s = use_signal(|| String::from("08:00"));
+    let mut now = use_signal::<Option<i32>>(|| None);
+    let mut times: Vec<i32> = lines
         .read()
         .split("\n")
-        .filter_map(|s| try_from(s).ok())
+        .filter_map(|s| parse_as_minutes(s).ok())
         .collect();
     if times.len() % 2 == 1 {
-        times.push(now());
+        times.push(now_minutes());
     }
     let elapsed = times.chunks_exact(2).map(|v| v[1] - v[0]).sum();
-    let goal = try_from(goal_s.read().as_str()).unwrap_or_default();
+    let goal = parse_as_minutes(goal_s.read().as_str()).unwrap_or_default();
     rsx! {
         TitleAndMeta {
             title: "時間計算するやつ",
@@ -42,37 +42,37 @@ pub fn TimeGoal() -> Element {
         main { class: "typography ui",
             h1 { class: "text-xl font-semibold", "目標まであと何分？" }
             hr { class: "my-3" }
-            div { class: "my-2",
+            div { class: "my-2 flex items-center gap-3",
                 "目標"
                 input {
-                    r#type: "text",
+                    class: "p-1",
+                    r#type: "time",
                     value: "{goal_s}",
                     oninput: move |e| *goal_s.write() = e.value(),
                 }
             }
             textarea {
-                class: "block",
-                value: "{s}",
+                class: "block p-2 w-full",
+                value: "{lines}",
                 rows: 4,
                 oninput: move |e| {
-                    *s.write() = e.value();
-                    *now_signal.write() = (!e.value().is_empty()).then(now);
+                    *lines.write() = e.value();
+                    *now.write() = (!e.value().is_empty()).then(now_minutes);
                 },
             }
-            if let Some(n) = now_signal() {
-                div {
-                    "いま: {fmt(n)}"
-                    br {}
-                    "経過時間: {fmt(elapsed)}"
-                    br {}
+            div { class: "mt-3",
+                if let Some(n) = now() {
+                    p { "いま: {fmt(n)}" }
+                    p { "経過時間: {fmt(elapsed)}" }
                     if goal > elapsed {
-                        "達成予定時刻: {fmt(n + goal - elapsed)}"
+                        p { "達成予定時刻: {fmt(n + goal - elapsed)}" }
+                        if times.len() == 2 {
+                            p { "休憩込み: {fmt(n + goal - elapsed + 60)}" }
+                        }
                     } else {
-                        "目標達成！"
+                        p { "目標達成！" }
                     }
                 }
-            } else {
-                "なにか入力してね"
             }
         }
     }
